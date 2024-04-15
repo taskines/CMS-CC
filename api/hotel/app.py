eb67a64931940f2983edd776f2e38f306beafb43
taskines <taskines@arcada.fi>
2024-04-15 06:10:37 +0000

    Commit before lesson 15.4.

--- api/hotel/app.py
+++ api/hotel/app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PORT=8811 #freddes port
+PORT=8811 # Freddes port ANVÄND DIN EGEN!
 
 db_url = os.environ.get("DB_URL")
 print(os.environ.get("FOO"))
